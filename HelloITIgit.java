c8e50815bf95243edfe7dd4841f54c8d6f6af5d5
mdanash97 <132082850+mdanash97@users.noreply.github.com>
2023-04-30 12:46:40 +0300

    fifth commit

---
+++ HelloITIgit.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import java.util.*;
+
+class HelloITI
+{
+	public static void main(String[] args){
+		
+		System.out.println("Hello ITI");
+		System.out.println("Hello ITI");
+		
+	}
+}
